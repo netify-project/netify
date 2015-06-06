059228d957c17420fa433e790d4474c70f9a3a20
Sandruniaster <sandra.wymyslowska@gmail.com>
2015-06-06 23:21:06 +0200

    Text Colors

---
+++ app/src/main/java/pl/edu/ug/aib/netify/fragment/AboutFragment.java
@@ -0,0 +1,26 @@
+package pl.edu.ug.aib.netify.fragment;
+
+
+
+import android.support.v4.app.Fragment;
+
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EFragment;
+
+
+import pl.edu.ug.aib.netify.R;
+
+
+@EFragment(R.layout.fragment_about)
+public class AboutFragment extends Fragment {
+
+
+    @AfterViews
+    void init(){
+    }
+
+
+
+
+}
